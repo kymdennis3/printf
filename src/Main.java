a3c54b49c964c415c6528a7bcd186d9c89b20b51
kymdennis3 <kymdennis3@gmail.com>
2022-11-08 09:12:05 +0300

    introduced printf of character

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /* printf() = an optional method to control, format, and display text to the console window
+                      two arguments = format string + (object/variable/value)
+                      %[flags] [precision] [width] [conversion-character]
+         */
+
+         boolean myBoolean = true;
+         String myString = "Kimtai";
+         int myInt = 23;
+         double myDouble = 1000;
+         char myChar = '&';
+
+        //System.out.printf("%b",myBoolean);
+        System.out.printf("%c",myChar);
     }
 }
