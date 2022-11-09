684a324ba08e361184872a8d91683c8f2f646877
kymdennis3 <kymdennis3@gmail.com>
2022-11-09 17:53:50 +0300

    1. Introduced printf of String 2. Commented out sout of boolean, integer, Double and character

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,14 @@ public class Main {
 
          boolean myBoolean = true;
          String myString = "Kimtai";
-         int myInt = 23;
+         int myInt = 37;
          double myDouble = 1000;
          char myChar = '&';
 
         //System.out.printf("%b",myBoolean);
         //System.out.printf("%c",myChar);
-        //System.out.printf("%d",myInt);
+        System.out.printf("%d",myInt);
         //System.out.printf("%f",myDouble);
-        System.out.printf("%s",myString);
+        //System.out.printf("%s",myString);
     }
 }
