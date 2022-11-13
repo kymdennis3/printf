85105f7388641d97d37e9f308a4d047f2abab65c
kymdennis3 <kymdennis3@gmail.com>
2022-11-13 19:37:49 +0300

    right justified (spaces in the middle)

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,15 @@ public class Main {
         /*[precision]
         sets number of digits of precision when outputting floating-point values
          */
-        System.out.printf("You have this much money %.1f",myDouble);
+
+        /*[flags]
+        adds an effect to output based om the flag added to format specifier
+          - : left-justify
+          + : output a plus (+) or minus (-) sign for a numeric value
+          0 : numeric values are zero-padded
+          , : comma grouping separator if numbers > 1000
+         */
+
+        System.out.printf("You have this much money %20f",myDouble);
     }
 }
